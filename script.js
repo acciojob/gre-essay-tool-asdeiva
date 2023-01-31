a6833cbd1ab3ffaaed8175bd5bcb5562e216d59d
DEIVA SIVAM <a.s.deiva@gmail.com>
2023-01-31 11:11:32 +0530

    done

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 let evaluatedText=document.getElementById("evaluatedText");
 let wordCount=document.getElementById("wordCount");
 
-evaluatedText.addEventListener("input",calculateWord
+evaluatedText.addEventListener("input",calculateWord)
 
 	function calculateWord() {
 		let word = evaluatedText.value;
-		letterCount.innerHTML = word.split(" ").length;;
+		wordCount.innerHTML = word.split(" ").length;;
 	}
